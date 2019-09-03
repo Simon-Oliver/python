5c28508a8c6e8ba6ef3e13a34a45107c45c6123b
Simon-Oliver <stauffer.simon@outlook.com>
2019-09-03 20:36:38 +1000

    explored some python functions

---
+++ app.py
@@ -0,0 +1,13 @@
+birth_year = input('What is your birth year? ')
+birth_year = int(birth_year)
+print(2019 - birth_year)
+
+name = "Jennifer"
+print(name[1:-1])
+
+
+firstName = 'Max'
+lastName = 'Muster'
+message = f'{firstName} [{lastName}] is a bracket guy!'
+
+print(len(message))

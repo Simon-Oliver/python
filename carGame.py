30464e912f61d59f7180d17ae219baa7ad340a2a
Simon-Oliver <stauffer.simon@outlook.com>
2019-09-04 20:12:43 +1000

    added nested if statement

--- carGame.py
+++ carGame.py
@@ -4,15 +4,24 @@ stop - to stop the car
 quit - to exit the game
 '''
 is_playing = True
+car_started = False
 
 while is_playing:
     command = input('> ').lower()
     if command == 'help':
         print(help_msg)
     elif command == 'start':
-        print('Car started!')
+        if car_started:
+            print('Car is already started!')
+        else:
+            car_started = True
+            print('Car started!')
     elif command == 'stop':
-        print('Car stopped!')
+        if not car_started:
+            print('Car is already stopped!')
+        else:
+            car_started = False
+            print('Car stopped!')
     elif command == 'quit':
         print('Quiting Game')
         is_playing = False

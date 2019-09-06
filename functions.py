92ca5e2cdfdaf1993c7f8880c6774e3eae5dc737
Simon-Oliver <stauffer.simon@outlook.com>
2019-09-06 16:44:37 +1000

    functions and return

--- functions.py
+++ functions.py
@@ -1,7 +1,14 @@
-def greet_user(name):
+def greet_user(name='Anonymous'):
     print('Hello and welcome!')
     print(f'How are you today {name}?')
 
 
-greet_user('Max')
+greet_user()
 greet_user('Sarah')
+
+
+def square(number):
+    return number * number
+
+
+print(square(8))

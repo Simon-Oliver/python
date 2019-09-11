6d6396af2e80d853d267012782a17b202a94318f
Simon-Oliver <stauffer.simon@outlook.com>
2019-09-11 18:59:53 +1000

    random and path module

--- modules.py
+++ modules.py
@@ -1,6 +1,12 @@
-import importMe
+from utils import find_max
 from importMe import import_me as test
 from importMe import import_me
+from ecommerce.shipping import calculate_shipping
+
+numbers = [0, 5, 2, 1, 6, 909, 10, 77, 100]
 
 print(test())
 print(import_me())
+
+print(find_max(numbers))
+print(find_max([]))
---
+++ random_test.py
@@ -0,0 +1,10 @@
+import random
+
+players = ['John', 'Mary', 'Max', 'Sarah']
+winner = random.choice(players)
+
+for i in range(3):
+    print(random.random())
+
+
+print(winner)

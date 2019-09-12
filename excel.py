fa75e16e7bc14e4d42559872487c76d30da5200c
Simon-Oliver <stauffer.simon@outlook.com>
2019-09-12 21:01:13 +1000

    excel file reading func

--- excel.py
+++ excel.py
@@ -1,29 +1,28 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook('test_price.xlsx')
-sheet = wb['Sheet 1']
-#cell = sheet['a1']
-cell = sheet.cell(2, 3)
 
-# print(cell.value)
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet 1']
 
-for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    corrected_price = cell.value * 0.9
-    corrected_price_cell = sheet.cell(row, 4)
-    corrected_price_cell.value = corrected_price
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
+
+    values = Reference(sheet,
+                       min_row=2,
+                       max_row=sheet.max_row,
+                       min_col=4,
+                       max_col=4)
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'f2')
+
+    wb.save(filename)
 
 
-values = Reference(sheet,
-                   min_row=2,
-                   max_row=sheet.max_row,
-                   min_col=4,
-                   max_col=4)
-
-chart = BarChart()
-chart.add_data(values)
-sheet.add_chart(chart, 'f2')
-
-wb.save('transactions.xlsx')
-print(corrected_price)
+process_workbook('test_price.xlsx')

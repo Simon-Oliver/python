eb5e0b67d1c7658ca67a771c0d81c74e103a792a
Simon Stauffer <stauffer.simon@outlook.com>
2019-09-21 17:56:47 +1000

    regex, tuples and sorting

--- regEx.py
+++ regEx.py
@@ -1,12 +1,23 @@
 import re
 
 fhand = open('/Users/rhiannongschwend 1/Desktop/Simon/code/python/code3/mbox-short.txt')
+obj = {}
+temp = []
+
 
 for line in fhand:
    # y = re.findall('^X-DSPAM-Confidence: (\S+)',line)
     y = re.findall('^From: .+@(\S+)',line)
     
     if not len(y) < 1:
-         print(y)
+        obj[y[0]] = obj.get(y[0], 0) + 1
 
 
+for key,value in obj.items():
+    temp.append((value,key))
+
+temp = sorted(temp, reverse=True)
+
+for value,key in temp:
+    print(key,value)
+    

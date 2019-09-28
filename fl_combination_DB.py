764e7a846816538a6531361621d380508504d447
Simon-Oliver <stauffer.simon@outlook.com>
2019-09-28 16:58:30 +1000

    added to sqlite

--- fl_combination_DB.py
+++ fl_combination_DB.py
@@ -1,5 +1,12 @@
 import json
 import re
+import sqlite3
+
+connect = sqlite3.connect('flavour.db')
+db = connect.cursor()
+
+db.execute('DROP TABLE IF EXISTS Pairing')
+db.execute('CREATE TABLE Pairing (name TEXT, category TEXT, pairing TEXT)')
 
 with open('combinations.json') as json_file:
     data = json.load(json_file)
@@ -7,11 +14,20 @@ with open('combinations.json') as json_file:
         obj ={}
         ing = []
 
-        name = re.findall('flavordb.\w*.(\w*)',n['name'])
+        name = re.findall('flavordb.\w*.(.*)',n['name'])
+        category = re.findall('flavordb.(\w*).\w*',n['name'])
 
         for i in n['imports']:
             ingredient = re.findall('flavordb.\w*.(\w*)',i)
             if len(ingredient) < 1: continue
             ing.append(ingredient[0])
-        obj['name'] = ing
+        obj['name'] = name[0]
+        obj['category'] = category[0]
+        obj['match'] = ing
+        db.execute('INSERT INTO Pairing (name, category, pairing) VALUES (?, ?, ?)', (name[0], category[0], str(ing)))
+        connect.commit()
         print(obj)
+
+
+
+

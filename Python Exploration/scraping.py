06b28151bf462bbd2687bf73ee4b91cde45b2a7a
Simon-Oliver <stauffer.simon@outlook.com>
2020-02-06 21:22:55 +0100

    exploring bs4

--- Python Exploration/scraping.py
+++ Python Exploration/scraping.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://automatetheboringstuff.com/2e/chapter0/'
+URL = 'https://www.internetlivestats.com/watch/websites/'
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(class_='main')
-print(results)
+results = soup.find(class_='rts-counter')
+num1 = soup.find(id='innercounterview-desc')
+num2 = num1.find('a')
+
+print(num2.text)

a99fa0fa1f04db5a048a7db8247cdec89c39fe2d
Simon-Oliver <stauffer.simon@outlook.com>
2020-02-07 21:37:42 +0100

    headless scarping of dynamic content with selenium

--- Python Exploration/scraping.py
+++ Python Exploration/scraping.py
@@ -1,12 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
-URL = 'https://www.internetlivestats.com/watch/websites/'
-page = requests.get(URL)
+browser_options = Options()  # Options Object from the Selenium driver.
+browser_options.add_argument(
+    '-headless')  # No GUI More refs at : https://developer.mozilla.org/en-US/Firefox/Headless_mode#Selenium_in_python
+browser = webdriver.Firefox(executable_path='geckodriver', options=browser_options)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(class_='rts-counter')
-num1 = soup.find(id='innercounterview-desc')
-num2 = num1.find('a')
 
-print(num2.text)
+browser.get('https://time.is')
+soup = BeautifulSoup(browser.page_source, "html.parser")
+
+num1 = soup.find(id="clock").find_all('span')
+
+for line in num1:
+    print(line.text)
+

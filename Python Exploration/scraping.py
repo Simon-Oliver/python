4942a5356337abd65c48aac0074ee27e32f473a8
Simon-Oliver <stauffer.simon@outlook.com>
2020-02-08 22:40:43 +0100

    scraping reader view of webpage and save as html

--- Python Exploration/scraping.py
+++ Python Exploration/scraping.py
@@ -3,22 +3,44 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+html = requests.get('https://automatetheboringstuff.com/2e/chapter0/')
 
-
-browser_options = Options()  # Options Object from the Selenium driver.
-browser_options.add_argument(
-    '-headless')  # No GUI More refs at : https://developer.mozilla.org/en-US/Firefox/Headless_mode#Selenium_in_python
-
-browser = webdriver.Chrome(executable_path='./chromedriver')
+# browser_options = Options()  # Options Object from the Selenium driver.
+# browser_options.add_argument(
+#     '-headless')  # No GUI More refs at : https://developer.mozilla.org/en-US/Firefox/Headless_mode#Selenium_in_python
+#
+# browser = webdriver.Chrome(executable_path='./chromedriver')
 # , options=browser_options
 # browser = webdriver.Firefox(executable_path='geckodriver', options=browser_options)
 
 
-browser.get('https://time.is')
-soup = BeautifulSoup(browser.page_source, "html.parser")
+# browser.get('https://time.is')
+soup = BeautifulSoup(html.content, "html.parser")
+# browser.page_source
 
-num1 = soup.find(id="clock").find_all('span')
+num1 = soup.find(class_="main")
+#
+# for line in num1:
+#     print(line.text)
 
-for line in num1:
-    print(line.text)
+tempText = '''
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
 
+<body>
+''' + str(num1) + '''
+</body>
+</html>
+'''
+f = open('scrapingHTML.html', "w+", encoding='utf-8')
+
+for line in tempText:
+    f.write(line)
+
+f.close()
+
+print('Done')
